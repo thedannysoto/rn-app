248818d4107cb283b24a100ad4f8231e726086c8
thedannysoto <87dsoto@gmail.com>
2020-06-29 15:35:45 -0500

    Adds a check for user pressing 'add' with an empty input

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ export default function App() {
   const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {
+      return;
+    }
     setMyGoals(currentGoals => [
       ...currentGoals, 
       { id: Math.random().toString(), value: goalTitle}
